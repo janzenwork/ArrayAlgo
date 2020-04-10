1f34401721d8c0d42b727ceaa19a67e570dfcd30
janzenwork <62186185+janzenwork@users.noreply.github.com>
2020-04-10 12:27:55 -0700

    .sln for visual studio and .cs files
    This displays the reverse of an array without using a placeholder array.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        public static void Main()
+        {
+            char[] phrase = new char[] { 'h','e','l','l','o' };
+
+            for (int i = phrase.Length - 1; i >= phrase.Length / 2; i--)
+            {
+                char temp = phrase[i];
+                phrase[i] = phrase[phrase.Length - 1 - i];
+                phrase[phrase.Length - 1 - i] = temp;
+            }
+            Console.Write(phrase);
+        }
+    }
+}
