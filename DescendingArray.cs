ff8668cbf3e8be24c26e614a89629f84ed954023
janzenwork <62186185+janzenwork@users.noreply.github.com>
2020-04-10 12:45:47 -0700

    Add files via upload
    This program contains a function that accepts an int array as a parameter, and displays the array in descending order.

---
+++ DescendingArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArrayDesc
+{
+    class Descending
+    {
+        static void Main()
+        {
+            int[] array = { 9, 3, 1, 2, 6, 5, 4, 7, 8, 4, 9 };
+            Desc(array);
+        }
+
+        public static void Desc(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int k = 1 + i; k< arr.Length; k++)
+                {
+                    if (arr[k] > arr[i])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
