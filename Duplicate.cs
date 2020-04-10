703330a90570fe44b9723646b1611e0cf6376c2e
janzenwork <62186185+janzenwork@users.noreply.github.com>
2020-04-10 12:54:37 -0700

    Add files via upload
    This program has a function that accepts an int array as a parameter, and checks for duplicate values in that array. If there are any duplicates the program ends automatically and if there aren't any duplicates the values in the array are displayed to the user.

---
+++ Duplicate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Duplicates
+{
+    class Dups
+    {
+        static void Main()
+        {
+            int[] array = { 9, 3, 1, 2, 6, 5, 4, 7, 8, 4, 9 };
+            DuplicateValues(array);
+        }
+
+        public static void DuplicateValues(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int k = 1 + i; k< arr.Length; k++)
+                {
+                    if (arr[k] == arr[i])
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
