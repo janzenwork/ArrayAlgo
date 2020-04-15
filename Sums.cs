f2b4e86bf3928f5253a16c7f10d73b4c41c5342c
janzenwork <62186185+janzenwork@users.noreply.github.com>
2020-04-15 16:25:19 -0700

    Add files via upload
    Function accepts and int[] array and a target int, and iterates through array to see if any two values in the array add up to the target value. returns indices if so, else it throws exception.

---
+++ Sums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] num = new[] { 1, 2, 3, 6, 7, 9 };
+            int target = 5;
+
+            Sums sum = new Sums();
+            sum.Sum(num, target);
+        }
+
+    }
+
+    public class Sums
+    {
+
+        public int[] Sum(int[] num, int target)
+        {
+            //Check which two numbers in array add up to target
+            //Returns array
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int k = 1 + i; k < num.Length; k++)
+                {
+                    if (target == num[i] + num[k])
+                    {
+                        return new int[]{ i, k };
+                    }
+                }
+            }
+
+            //throw exception because all code paths must return a value.
+            throw new System.ArgumentException("No two values add up to target.");
+        }
+    }
+}
