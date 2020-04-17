df847e7cb2895b504621547cf2f1aa1d39b278cc
Janzen <62186185+janzenwork@users.noreply.github.com>
2020-04-16 17:30:21 -0700

    RemovesValuesFromArray
    Function takes an int[] and an int 'value' as parameters. It counts every value in the array that is not equal to the given 'value' and writes the new Length.

---
+++ RemoveValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RemoveElements
+{
+    class Elements
+    {
+        static void Main()
+        {
+
+            int[] nums = { 3, 2, 2, 4,3,3,4,5,6,10,8,9 };
+            int val = 3;
+            remove(nums, val);
+
+        }
+
+        public static void remove(int[] nums, int val)
+        {
+
+            int temp = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[temp] = nums[i];
+                    temp++;
+                }
+            }
+            Console.Write(temp);
+        }
+    }
+}
